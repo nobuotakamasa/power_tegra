dc691f2fd0d68f42c359cfb0d0596ec6cde9a486
autoware <nobuo.takamasa@tier4.jp>
2024-07-22 22:37:44 +0900

    first commit

---
+++ PowerRead.py
@@ -0,0 +1,10 @@
+import subprocess
+
+command = ["tegrastats", "--interval", "1000" "--verbose"]
+process = subprocess.Popen(command, stdout = subprocess.PIPE, stderr=subprocess.PIPE)
+while True:
+	output = process.stdout.readline()
+	output = output.strip()
+	output = str(output).split(' ')
+	print(output)
+
