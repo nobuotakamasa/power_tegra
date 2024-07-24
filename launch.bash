28f6c211da5737da6d8e27b4a11a1df97134a144
Nobuo TAKAMASA <nobuo.takamasa@tier4.jp>
2024-07-24 23:47:58 +0900

    for test

--- launch.bash
+++ launch.bash
@@ -1,4 +1,8 @@
 source ./dds.bash
 source ./install/setup.bash
 sudo echo ""
+ARCH=$(uname-m)
+if ["$ARCH" = "x86_64"]; then
+    export PATH=./:$PATH
+fi
 ros2 launch power_publisher power_publisher.launch.py interval:=1000 ecu_name:=rqx58g
