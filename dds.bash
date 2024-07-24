93a2b3386916315bf3c71deb70055149e1eb9589
Nobuo TAKAMASA <nobuo.takamasa@tier4.jp>
2024-07-24 10:39:33 +0900

    adding setup for dds

---
+++ dds.bash
@@ -0,0 +1,6 @@
+export ROS_DOMAIN_ID=100
+export RCUTILS_COLORIZED_OUTPUT=1
+export RCUTILS_CONSOLE_OUTPUT_FORMAT="[{severity} {time}] [{name}] {function_name}(): {message}"
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export CYCLONEDDS_URI=file:///home/autoware/cyclonedds.xml
+
--- launch.bash
+++ launch.bash
@@ -1,3 +1,4 @@
+source ./dds.bash
 source ./install/setup.bash
 sudo echo ""
 ros2 launch power_publisher power_publisher.launch.py interval:=1000 ecu_name:=rqx58g
