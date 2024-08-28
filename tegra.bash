d6809f1edf36cf0913e7e456d877e37d33c5bca9
NOBUO Takamasa <nobuo.takamasa@tier4.jp>
2024-08-28 14:27:13 +0900

    adding topic to host name

--- tegra.bash
+++ tegra.bash
@@ -3,8 +3,11 @@ source ./install/setup.bash
 sudo echo ""
 ARCH=$(uname -m)
 if [ "$ARCH" = "x86_64" ]; then
+    #for test
     export PATH="./:$PATH"
 fi
-echo $ARCH
-echo $PATH
-ros2 launch power_publisher tegra_publisher.launch.py interval:=1000 ecu_name:=rqx58g
+echo "arch=$ARCH"
+HOSTNAME=$(uname -n)
+echo "hostname=$HOSTNAME"
+#echo $PATH
+ros2 launch power_publisher tegra_publisher.launch.py interval:=1000 ecu_name:=$HOSTNAME
